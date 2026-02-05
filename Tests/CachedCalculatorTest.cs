06640623dcd42ae87a5820c838c4ea088870d707
hund555 <allanholmiversen@gmail.com>
2026-02-05 10:20:54 +0100

    Added unit tests

--- Tests/CachedCalculatorTest.cs
+++ Tests/CachedCalculatorTest.cs
@@ -18,4 +18,74 @@ public class CachedCalculatorTest
         // Assert
         Assert.That(result, Is.EqualTo(5));
     }
+
+    [Test]
+    public void Subtract()
+    {
+        // Arrange
+        ICalculator calc = new CachedCalculator();
+        int a = 5, b = 2;
+
+        // Act
+        int result = calc.Subtract(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Multiply()
+    {
+        // Arrange
+        ICalculator calc = new CachedCalculator();
+        int a = 2, b = 2;
+
+        // Act
+        int result = calc.Multiply(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(4));
+    }
+
+    [Test]
+    public void Divide()
+    {
+        // Arrange
+        ICalculator calc = new CachedCalculator();
+        int a = 6, b = 2;
+
+        // Act
+        int result = calc.Divide(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Factorial1()
+    {
+        // Arrange
+        ICalculator calc = new CachedCalculator();
+        int a = 5;
+
+        // Act
+        int result = calc.Factorial(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(120));
+    }
+
+    [Test]
+    public void Factorial2()
+    {
+        // Arrange
+        ICalculator calc = new CachedCalculator();
+        int a = 0;
+
+        // Act
+        int result = calc.Factorial(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
 }
